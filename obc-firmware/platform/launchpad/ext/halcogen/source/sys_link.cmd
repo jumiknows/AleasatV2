3c323bd8d26a3af6ebe08b0750f07c7e5bc5d222
Noah Tajwar <noah.tajwar@ubcorbit.com>
2023-11-15 12:47:22 +0000

    Fixed stack sizes on LaunchPad https://gitlab.com/alea-2020/command-data-handling/obc2-firmware/-/merge_requests/202
    # Overview
    This was a fun one.
    
    Symptom: The stack usage of the `CMD_SYS_EXEC` task was really high only on the LaunchPad.
    
    Root Cause:
    - There is a HALCoGen configuration that determines how much stack space each processor mode gets (effectively by setting the initial value of the stack pointer for each processor mode - see `_coreInitStackPointer_` in `sys_core.asm`).
      - Note: these are totally independent from the RAM allocated for each FreeRTOS task's stack.
    - Our linker script was assuming all the processor mode stacks collectively take up the first 0x800 bytes of RAM which is true for the alea-v1 HALCoGen, but for some reason the LaunchPad HALCoGen had the stacks configured to take up 0x1500 bytes of RAM so the supervisor mode stack (among others) overlapped with the beginning of the USER RAM region.
    - It just so happens that the linker had decided to allocate the FreeRTOS task stack for the `CMD_SYS_EXEC` task at the beginning of the USER RAM region. Since the supervisor mode stack overlapped with this, anytime a supervisor call was made (i.e. anytime we call a FreeRTOS API) the low addresses of the `CMD_SYS_EXEC` task would get clobbered.
      - It also just so happens that the `CMD_SYS_EXEC` task is very large and grows downwards, so it never reached these clobbered addresses and we never saw any real issues
      - EXCEPT: Checking the stack high watermark is done by filling the stack with a known value at task creation time and looking for the lowest address where that value has since been modified
        - Since the supervisor calls were modifying the low addresses of the `CMD_SYS_EXEC` task stack, it seemed like that's where the high watermark was, when in-fact barely any of the stack had been used.
    
    # Testing
    Basic manual testing on the LaunchPad to make sure startup succeeds and commands work

--- obc-firmware/platform/launchpad/ext/halcogen/source/sys_link.cmd
+++ obc-firmware/platform/launchpad/ext/halcogen/source/sys_link.cmd
@@ -79,7 +79,7 @@ MEMORY
 {
     VECTORS (X)  : origin=0x00000000 length=0x00000020
     FLASH0  (RX) : origin=0x00000020 length=0x0013FFE0
-    STACKS  (RW) : origin=0x08000000 length=0x00001500
+    STACKS  (RW) : origin=0x08000000 length=0x00000800
     RAM     (RW) : origin=0x08001500 length=0x0002EB00
 
 /* USER CODE BEGIN (2) */
